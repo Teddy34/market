edcc84bf8e85266d014c9b3f38849bfa30f10e38
JordiTeddy <jorditeddy@gmail.com>
2016-04-18 23:05:03 +0200

    small improvement of testBase

--- test/testBase.js
+++ test/testBase.js
@@ -1,8 +1,8 @@
 var sdeConnector = require('../io/sdeConnector');
 var tools = require('../tools');
 
-var getSystemIDFromSystemName = function() {
-  return sdeConnector.sendQueryWhenReady('SELECT s."solarSystemID", s."solarSystemName" FROM "mapSolarSystems" AS "s" WHERE "solarSystemName" = \''+'Jita'+'\'');
+var getSystemIDFromSystemName = function(systemName) {
+  return sdeConnector.sendQueryWhenReady('SELECT s."solarSystemID", s."solarSystemName" FROM "mapSolarSystems" AS "s" WHERE "solarSystemName" = \''+systemName+'\'');
 };
 
 var getLocationsFromSystemID = function(systemID) {
@@ -13,4 +13,4 @@ var getLocationsFromSystemName = function(systemName) {
   return sdeConnector.sendQueryWhenReady('SELECT s."solarSystemID", s."solarSystemName", t."stationID", s."constellationID", s."regionID" FROM "staStations" AS "t" INNER JOIN "mapSolarSystems" s ON s."solarSystemID"=t."solarSystemID" WHERE "solarSystemName" = \''+systemName+'\'');
 };
 
-sdeConnector.connect().then(getSystemIDFromSystemName).then(tools.logResult,tools.logError);
+sdeConnector.connect().then(function(){return getSystemIDFromSystemName("Fliet")}).then(tools.logResult,tools.logError);
